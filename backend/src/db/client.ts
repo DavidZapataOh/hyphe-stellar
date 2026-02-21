8675f44e36fba6bf95d6ea0fccc9615ec0fd0ef9
David Zapata <davidzapata5885@gmail.com>
2026-02-20 23:59:41 -0500

    fix: client bug

--- backend/src/db/client.ts
+++ backend/src/db/client.ts
@@ -1,8 +1,9 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
 const adapter = new PrismaPg({
-  connectionString: process.env["DATABASE_URL"] || "postgresql://user:pass@localhost:5432/hyphe",
+  connectionString: process.env["DATABASE_URL"]!,
 });
 
 export const prisma = new PrismaClient({ adapter });
